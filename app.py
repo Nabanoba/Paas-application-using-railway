4caa7bd44bf537a6c2d6945e332b4053b3277118
Nabanoba <nanyunjayunia@gmail.com>
2026-04-02 19:54:02 +0300

    Ready for Railway deployment

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def delete(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Ensure tables are created
-    app.run(debug=True, host="127.0.0.1", port=5000)
+app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
