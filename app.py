a4d32c4d355546ffa9008328ccb10b1cf809638a
Nabanoba <nanyunjayunia@gmail.com>
2026-04-03 12:43:39 +0300

    Deployment-ready app.py with dummy data

--- app.py
+++ app.py
@@ -81,10 +81,5 @@ def test_db():
 # -------------------------------
 # Run App
 # -------------------------------
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Ensure tables are created
-
-    port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("FLASK_DEBUG", "False") == "True"
-    app.run(debug=debug, host="0.0.0.0", port=port)
+with app.app_context():
+    db.create_all()  # Ensure tables are created
